da411ff1a43ac96333618e2148e73412cb1775d2
lokesh1306 <challagulla.l@northeastern.edu>
2022-12-06 12:09:29 -0500

    DB

---
+++ Backend/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
