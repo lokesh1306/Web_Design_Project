4f8b83ef443655174578010dedd4b8aaa8aa76ea
Venkat2041 <114768456+Venkat2041@users.noreply.github.com>
2022-12-07 13:36:45 -0500

    update

---
+++ frontend/src/components/index.js
@@ -0,0 +1,88 @@
+import React from "react";
+import {
+  Home,
+  WishList,
+  ProtectedRoute,
+  AdminProtectedRoute,
+  CartProtectedRoute,
+  PageNotFound,
+  ProductDetails,
+  ProductByCategory,
+  CheckoutPage,
+} from "./portal";
+import { DashboardAdmin, Categories, Products, Orders } from "./admin";
+import { UserProfile, UserOrders, SettingUser } from "./portal/dashboardUser";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+/* Routing All page will be here */
+const Routes = (props) => {
+  return (
+    <Router>
+      <Switch>
+        {/* Portal & Public Routes */}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/wish-list" component={WishList} />
+        <Route exact path="/products/:id" component={ProductDetails} />
+        <Route path="/privacy-policy" render={() => window.location = "http://127.0.0.1:5500/src/Travio/index.html"} />
+        <Route
+          exact
+          path="/products/category/:catId"
+          component={ProductByCategory}
+        />
+        <CartProtectedRoute
+          exact={true}
+          path="/checkout"
+          component={CheckoutPage}
+        />
+        {/* Portal & Public Routes End */}
+
+        {/* Admin Routes */}
+        <AdminProtectedRoute
+          exact={true}
+          path="/admin/dashboard"
+          component={DashboardAdmin}
+        />
+        <AdminProtectedRoute
+          exact={true}
+          path="/admin/dashboard/categories"
+          component={Categories}
+        />
+        <AdminProtectedRoute
+          exact={true}
+          path="/admin/dashboard/products"
+          component={Products}
+        />
+        <AdminProtectedRoute
+          exact={true}
+          path="/admin/dashboard/orders"
+          component={Orders}
+        />
+        {/* Admin Routes End */}
+
+        {/* User Dashboard */}
+        <ProtectedRoute
+          exact={true}
+          path="/user/profile"
+          component={UserProfile}
+        />
+        <ProtectedRoute
+          exact={true}
+          path="/user/orders"
+          component={UserOrders}
+        />
+        <ProtectedRoute
+          exact={true}
+          path="/user/setting"
+          component={SettingUser}
+        />
+        {/* User Dashboard End */}
+
+        {/* 404 Page */}
+        <Route component={PageNotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
