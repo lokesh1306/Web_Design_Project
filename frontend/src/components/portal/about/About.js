30b7817c0d8ea454d91020d6db570af5fb9326fb
balajiarulselvan <sothiselvam.b@northeastern.edu>
2022-12-09 17:14:06 -0500

    about us

--- frontend/src/components/portal/about/About.js
+++ frontend/src/components/portal/about/About.js
@@ -9,7 +9,7 @@ const AboutComponent = (props) => {
       
         <div className="about-section">
   <h1>About Us Page</h1>
-  <p>Some text about who we are and what we do.</p>
+  <p>We help you design your holidays</p>
   <p>Resize the browser window to see that this page is responsive by the way.</p>
 </div>
 <h2>Our Team</h2>
