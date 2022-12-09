52327ff7e9f6d0d02d80f199e87d5c9f3ac9914f
lokesh1306 <challagulla.l@northeastern.edu>
2022-12-09 18:36:09 -0500

    FAQ changes

--- frontend/src/components/portal/home/faq.js
+++ frontend/src/components/portal/home/faq.js
@@ -7,109 +7,22 @@ const Faqcomponent = (props) => {
     <div className="flex flex-col items-center justify-center my-16">
       <br />
       <div class="accordion" id="accordionExampleY">
-      <div class="accordion-item">
-        <div className="faq-section">
-          <h1>FAQ</h1>
-        <h2 class="accordion-header" id="headingOneY">
-          <button class="accordion-button" type="button" data-mdb-toggle="collapse"
-            data-mdb-target="#collapseOneY" aria-expanded="true" aria-controls="collapseOneY">
-            <i class="fas fa-question-circle fa-sm me-2 opacity-70">Which payment methods are acceptable?</i>
-          </button>
-        </h2>
-        <div id="collapseOneY" class="accordion-collapse collapse show" aria-labelledby="headingOneY"
-          data-mdb-parent="#accordionExampleY">
-          <div class="accordion-body">
-            <strong>We accept PayPal payments, Discover, Mastercard, Visa or American Express.</strong>
-          </div>
-        </div>
-      </div>
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="headingTwoY">
-          <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
-            data-mdb-target="#collapseTwoY" aria-expanded="false" aria-controls="collapseTwoY">
-            <i class="fas fa-question-circle fa-sm me-2 opacity-70">How do I obtain Essential Trip Information with full details of my trip?</i>
-          </button>
-        </h2>
-        <div id="collapseTwoY" class="accordion-collapse collapse" aria-labelledby="headingTwoY"
-          data-mdb-parent="#accordionExampleY">
-          <div class="accordion-body">
-            <strong>Make sure you bring these with you. Every trip has a set of Essential Trip Information, accessible on our website, that gives you in-depth information about all aspects of your trip. To access your Essential Trip Information, please visit your trip’s specific facts page on our website. The Essential Trip Information are accessible at the top of this page.</strong>
-          </div>
-        </div>
-      </div>
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="headingThreeY">
-          <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
-            data-mdb-target="#collapseThreeY" aria-expanded="false" aria-controls="collapseThreeY">
-            <i class="fas fa-question-circle fa-sm me-2 opacity-70">Is it possible to manage details through dashboard?</i>
-          </button>
-        </h2>
-        <div id="collapseThreeY" class="accordion-collapse collapse" aria-labelledby="headingThreeY"
-          data-mdb-parent="#accordionExampleY">
-          <div class="accordion-body">
-            <strong>Yes. You simply need to be logged in and go to your dashboard. There you’ll be able to do things like update your calendar to show your next availability, adjust your notification preferences, change your details and update your preferences.</strong> 
-          </div>
-        </div>
-      </div>
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="headingFourY">
-          <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse" data-mdb-target="#collapseFourY" aria-expanded="false" aria-controls="collapseFourY">
-            <i class="fas fa-question-circle fa-sm me-2 opacity-70">Multiple tour bookings are allowed?</i>
-          </button>
-        </h2>
-        <div id="collapseFourY" class="accordion-collapse collapse" aria-labelledby="headingFourY"
-          data-mdb-parent="#accordionExampleY">
-          <div class="accordion-body">
-            <strong>Yes. To travel on any tour, you must be deemed Good to Go. If you have multiple tours on one booking, you only need to go through the process once</strong> 
-          </div>
-        </div>
-      </div>
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="headingFiveY">
-          <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse" data-mdb-target="#collapseFiveY" aria-expanded="false" aria-controls="collapseFiveY">
-            <i class="fas fa-question-circle fa-sm me-2 opacity-70">I want to cancel my booking?</i>
-          </button>
-        </h2>
-        <div id="collapseFiveY" class="accordion-collapse collapse" aria-labelledby="headingFiveY"
-          data-mdb-parent="#accordionExampleY">
-          <div class="accordion-body">
-            <strong>Reach out to us right away. If it is beyond usual business hours, kindly contact the following travel agency to cancel any reservations you won't be able to maintain. To start the cancellation process and make sure you receive the full refund, please call the provider's 24-hour number.</strong> 
-          </div>
-        </div>
-      </div>
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="headingFiveY">
-          <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse" data-mdb-target="#collapseFiveY" aria-expanded="false" aria-controls="collapseFiveY">
-            <i class="fas fa-question-circle fa-sm me-2 opacity-70">All taxes are included in the booking prices?</i>
-          </button>
-        </h2>
-        <div id="collapseFiveY" class="accordion-collapse collapse" aria-labelledby="headingFiveY"
-          data-mdb-parent="#accordionExampleY">
-          <div class="accordion-body">
-            <strong>The price you provide must include the base rate and all taxes and fees are included in the booking prices.Taxes and fees must represent all mandatory charges collected by the partner.</strong> 
-          </div>
-        </div>
-      </div>
-      </div>
-      </div>
-      <div class="accordion" id="accordionExampleY">
   <div class="accordion-item">
+    <div className="faq-section">
+      <h3>Frequently asked questions</h3>
+   
     <h2 class="accordion-header" id="headingOneY">
       <button class="accordion-button" type="button" data-mdb-toggle="collapse"
         data-mdb-target="#collapseOneY" aria-expanded="true" aria-controls="collapseOneY">
-        <i class="fas fa-question-circle fa-sm me-2 opacity-70"></i>Which payment methods are acceptable?
+        <i class="fas fa-question-circle fa-sm me-2 opacity-70"></i>WHY SHOULD I USE A TRAVEL AGENT TO BOOK A VACATION?
       </button>
     </h2>
     <div id="collapseOneY" class="accordion-collapse collapse show" aria-labelledby="headingOneY"
       data-mdb-parent="#accordionExampleY">
       <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is hidden by
-        default, until the collapse plugin adds the appropriate classes that we use to
-        style each element. These classes control the overall appearance, as well as
-        the showing and hiding via CSS transitions. You can modify any of this with
-        custom CSS or overriding our default variables. It's also worth noting that
-        just about any HTML can go within the <code>.accordion-body</code>, though the
-        transition does limit overflow.
+        <strong>A travel agent takes all of the headache out of planning a trip and handles virtually all aspects of your travel, at absolutely no cost to you. The combined experience of the team and the feedback from our thousands of travelers provides an invaluable resource to the traveler planning an important trip.
+
+In recent years, the top travel vendors have empowered and trained the agency community to be even more effective and efficient go-betweens. </strong> 
       </div>
     </div>
   </div>
@@ -117,19 +30,15 @@ const Faqcomponent = (props) => {
     <h2 class="accordion-header" id="headingTwoY">
       <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
         data-mdb-target="#collapseTwoY" aria-expanded="false" aria-controls="collapseTwoY">
-        <i class="fas fa-question-circle fa-sm me-2 opacity-70"></i>Accordion Item #2
+        <i class="fas fa-question-circle fa-sm me-2 opacity-70"></i>DO I NEED TRAVEL INSURANCE?
       </button>
     </h2>
     <div id="collapseTwoY" class="accordion-collapse collapse" aria-labelledby="headingTwoY"
       data-mdb-parent="#accordionExampleY">
       <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by
-        default, until the collapse plugin adds the appropriate classes that we use to
-        style each element. These classes control the overall appearance, as well as
-        the showing and hiding via CSS transitions. You can modify any of this with
-        custom CSS or overriding our default variables. It's also worth noting that
-        just about any HTML can go within the <code>.accordion-body</code>, though the
-        transition does limit overflow.
+        <strong>Travel insurance is available and recommended to protect your travel investment. Many trips, such as tours and cruises, involve non-refundable funds paid months in advance. Travel insurance protects you from losing this investment. Perhaps even more important is the ‘during travel’ coverage and assistance that comes with the top-flight insurance companies we carry.
+
+</strong> 
       </div>
     </div>
   </div>
@@ -137,23 +46,20 @@ const Faqcomponent = (props) => {
     <h2 class="accordion-header" id="headingThreeY">
       <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
         data-mdb-target="#collapseThreeY" aria-expanded="false" aria-controls="collapseThreeY">
-        <i class="fas fa-question-circle fa-sm me-2 opacity-70"></i>Accordion Item #3
+        <i class="fas fa-question-circle fa-sm me-2 opacity-70"></i>HOW DO I KNOW I CAN TRUST THIS TRAVEL VENDOR?
       </button>
     </h2>
     <div id="collapseThreeY" class="accordion-collapse collapse" aria-labelledby="headingThreeY"
       data-mdb-parent="#accordionExampleY">
       <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by
-        default, until the collapse plugin adds the appropriate classes that we use to
-        style each element. These classes control the overall appearance, as well as
-        the showing and hiding via CSS transitions. You can modify any of this with
-        custom CSS or overriding our default variables. It's also worth noting that
-        just about any HTML can go within the <code>.accordion-body</code>, though the
-        transition does limit overflow.
+        <strong>Boston Travels a Direct Travel Company is extremely cautious when it comes to choosing our business partners and vendors. This vetting of travel suppliers and support of the ‘good ones’ over the past 50+ years has given us preferred access to the world’s top trusted suppliers. In turn, they trust us to represent and care for the reputations they have earned over the years and to deliver the same high standard of care to our mutual clients.
+
+</strong>
       </div>
     </div>
   </div>
-</div>
+</div> </div>
+     
     </div>
   );
 };
